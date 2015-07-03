a7731e868b69477df5657bc531c60bbbbf34bb96
Ondrej Bartas <ondrej@bartas.cz>
2015-07-03 12:16:54 +0200

    Use puma configuration on heroku

---
+++ config/puma.rb
@@ -0,0 +1,14 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 1)
+min_threads = Integer(ENV['MIN_THREADS'] || 3)
+max_threads = Integer(ENV['MAX_THREADS'] || 8)
+threads min_threads, max_threads
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
