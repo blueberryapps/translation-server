d0b324303839042042324b590b5b8427f0320b8b
Ondrej Bartas <ondrej@bartas.cz>
2015-09-30 16:24:01 +0200

    Reformat Invalid HTML in string translations

--- spec/models/translation_spec.rb
+++ spec/models/translation_spec.rb
@@ -3,4 +3,51 @@ require 'rails_helper'
 RSpec.describe Translation, type: :model do
   it { should belong_to :locale }
   it { should belong_to :key }
+
+  describe '#text=' do
+    let(:data_type) { 'string' }
+    let(:key)       { Key.new(data_type: data_type)}
+
+    subject do
+      Translation.new(key: key, text: text)
+    end
+
+    context 'sets text to integer' do
+      let(:data_type) { 'integer' }
+      let(:text)      { 1 }
+      specify         { expect(subject.text).to eq('1') }
+    end
+
+    context 'sets text to string' do
+      let(:text) { 'hello' }
+      specify    { expect(subject.text).to eq('hello') }
+    end
+
+    context 'does not update valid html' do
+      let(:text) { '<i>valid</i>' }
+      specify    { expect(subject.text).to eq('<i>valid</i>') }
+    end
+
+    context 'fixes invalid html' do
+      let(:text) { '<i>valid</b>' }
+      specify    { expect(subject.text).to eq('<i>valid</i>') }
+    end
+
+    context 'does not remove empty element from valid html' do
+      let(:text) { '<span class="hi" /><b>valid</b>' }
+
+      specify do
+        expect(subject.text).to eq('<span class="hi"></span><b>valid</b>')
+      end
+    end
+
+    context 'pretty print of valid html' do
+      let(:text) { '<p class="hi"><b class="bye">valid</b></p>' }
+
+      specify do
+        expect(subject.text)
+          .to eq("<p class=\"hi\">\n  <b class=\"bye\">valid</b>\n</p>")
+      end
+    end
+  end
 end
