05b19885a092aef792ce8c6c24d5c9208f8b54f9
Ondrej Bartas <ondrej@bartas.cz>
2015-10-23 14:40:52 +0200

    Add rake for clear database

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -3,18 +3,6 @@ class RootController < ApplicationController
     @locales = policy_scope(Locale) if current_user
   end
 
-  # TODO: REMOVE THIS
-  def delete
-    Translation.delete_all
-    Highlight.delete_all
-    Image.delete_all
-    Location.delete_all
-    Key.delete_all
-    Release.delete_all
-    Locale.delete_all
-    redirect_to root_url
-  end
-
   def is_alive
     render text: 'Yeah Treanslation server is alive'
   end
