b09c35fbfd183560de3c39914230b53112e8fe07
Michal Juriš <michaljuris@gmail.com>
2016-10-24 23:42:49 +0200

    Add restricted ips administation

--- app/models/restricted_ip.rb
+++ app/models/restricted_ip.rb
@@ -1,7 +1,19 @@
 class RestrictedIp < ActiveRecord::Base
   belongs_to :user
 
+  validates_presence_of :ip
+
   scope :containing, -> (ip) { where('? <<= ip', ip) }
+
+  def cidr_prefix
+    # ruby IPAddr library doesn't provide any method to get prefix/subnet
+    # so this rather crude workaround has to be used
+    ip.instance_variable_get(:@mask_addr).to_s(2).count('1')
+  end
+
+  def cidr_ip
+    cidr_prefix == 32 ? ip.to_s : "#{ip.to_s}/#{cidr_prefix}"
+  end
 
   def self.contains?(ip)
     return true unless containing(ip).empty?
