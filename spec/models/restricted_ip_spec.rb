ca7b9e593caae16c0bfd03b16f7e53928843989f
Michal Juriš <michaljuris@gmail.com>
2016-10-26 13:22:37 +0200

    Refactor tests

--- spec/models/restricted_ip_spec.rb
+++ spec/models/restricted_ip_spec.rb
@@ -30,17 +30,60 @@ RSpec.describe RestrictedIp, type: :model do
     end
   end
 
-  describe '.contains? will return' do
-    before do
-      create :restricted_ip, ip: '192.168.1.0/24'
-    end
+  describe '.contains?' do
+    before { create :restricted_ip, ip: ip }
 
-    context 'true when ip is contained in restricted ips and ranges' do
+    context 'for IPv4 address range' do
+      let(:ip) { '192.168.1.0/24' }
+
       specify { expect(RestrictedIp.contains?('192.168.1.1')).to be_truthy }
+      specify { expect(RestrictedIp.contains?('192.168.1.250')).to be_truthy }
+      specify { expect(RestrictedIp.contains?('192.168.99.1')).to be_falsey }
+      specify { expect(RestrictedIp.contains?('172.22.0.1')).to be_falsey }
     end
 
-    context 'false when ip isnt contained in restricted ips or ranges' do
-      specify { expect(RestrictedIp.contains?('192.168.99.1')).to be_falsey }
+    context 'for single IPv4 address' do
+      let(:ip) { '192.168.1.50' }
+
+      specify { expect(RestrictedIp.contains?('192.168.1.50')).to be_truthy }
+      specify { expect(RestrictedIp.contains?('192.168.1.60')).to be_falsey }
+      specify { expect(RestrictedIp.contains?('172.22.0.1')).to be_falsey }
+    end
+
+    context 'for IPv6 address range' do
+      let(:ip) { '2001:1620:28::/48' }
+
+      specify do
+        expect(RestrictedIp.contains?('2001:1620:28:1:b6f:8bca:93:a116'))
+          .to be_truthy
+      end
+
+      specify do
+        expect(RestrictedIp.contains?('2001:1620:99:1:b6f:8bca:93:1'))
+          .to be_falsey
+      end
+
+      specify do
+        expect(RestrictedIp.contains?('fda0:a76a:68ed:1:1:1:1:1'))
+          .to be_falsey
+      end
+    end
+
+    context 'for single IPv6 address' do
+      let(:ip) { '2001:1620:28:1:b6f:8bca:93:a116' }
+
+      specify do
+        expect(RestrictedIp.contains?('2001:1620:28:1:b6f:8bca:93:a116'))
+          .to be_truthy
+      end
+      specify do
+        expect(RestrictedIp.contains?('2001:1620:28:1:b6f:8bca:93:1'))
+          .to be_falsey
+      end
+      specify do
+        expect(RestrictedIp.contains?('fda0:a76a:68ed:1:1:1:1:1'))
+          .to be_falsey
+      end
     end
   end
 end
