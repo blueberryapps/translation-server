5567f930d47f31e1be804e4f0781b769f8b2b91d
Ondrej Bartas <ondrej@bartas.cz>
2016-11-11 15:34:26 +0100

    Allow accept text/event stream

--- phoenix_changes/web/router.ex
+++ phoenix_changes/web/router.ex
@@ -2,7 +2,7 @@ defmodule PhoenixChanges.Router do
   use PhoenixChanges.Web, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
+    plug :accepts, ["html", "text/event-stream"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
