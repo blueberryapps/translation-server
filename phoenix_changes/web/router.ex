096152e403eb3a936dcf3ca893a6373269f23089
Ondrej Bartas <ondrej@bartas.cz>
2016-11-11 14:30:40 +0100

    Fix route for phoenix app

--- phoenix_changes/web/router.ex
+++ phoenix_changes/web/router.ex
@@ -16,7 +16,7 @@ defmodule PhoenixChanges.Router do
   scope "/", PhoenixChanges do
     pipe_through :browser # Use the default browser stack
 
-    get "/", ChangesController, :index
+    get "/api/v1/changes", ChangesController, :index
   end
 
   # Other scopes may use custom stacks.
