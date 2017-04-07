aa5e7ba5090c189179b69b0f64fa2d2e926c42fd
Albert Němec <albert.nemec2457@gmail.com>
2017-04-07 16:35:21 +0200

    connect babel & add default State

--- app/controllers/react_controller.rb
+++ app/controllers/react_controller.rb
@@ -1,6 +1,13 @@
 class ReactController < AuthController
   layout 'react'
   def index
-    @initial_state = 'initial state'
+    @initial_state = {
+      user: {
+        admin: current_user.admin?,
+        email: current_user.email,
+        username: current_user.username,
+        photo: current_user.photo_url(20)
+      }
+    }
   end
 end
--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,16 @@ const fs = require('fs');
 const browserify = require('browserify');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
+const babelrc = JSON.parse(fs.readFileSync('./.babelrc'));
 
 function swallowError(error) {
-  console.log(error.stack) // eslint-disable-line
+  console.log(error.stack); // eslint-disable-line
   this.emit('end');
 }
 
 function build() {
   return browserify('./app/react/browser/main.js')
-    .transform(babelify.configure({
-      presets: ['react', 'es2015', 'stage-1', 'stage-2']
-    }))
+    .transform(babelify.configure(babelrc))
     .bundle()
     .on('error', swallowError)
     .pipe(fs.createWriteStream('./app/assets/javascripts/react.js'));
@@ -27,20 +26,19 @@ function watch() {
     cache: {}, // required for watchify
     packageCache: {}, // required for watchify
     debug: process.env.DEBUG,
-    plugin: [watchify]
+    plugin: [watchify],
   });
-  b.transform(babelify.configure({
-    presets: ['react', 'es2015', 'stage-1', 'stage-2'],
-    plugins: ['transform-decorators-legacy']
-  }));
+  b.transform(babelify.configure(babelrc));
 
   function rebuild() {
-    const startedAt = new Date
-    console.log('Build started at ' + startedAt) //eslint-disable-line
-    b.bundle()
+    const startedAt = new Date();
+    console.log('Build started at ' + startedAt); //eslint-disable-line
+    b
+      .bundle()
       .on('error', swallowError)
       .pipe(fs.createWriteStream('./app/assets/javascripts/react.js'))
-      .on('finish', () => console.log('Finished in ' + (new Date().getTime() - startedAt.getTime()) + 'ms')) //eslint-disable-line
+      .on('finish', () =>
+        console.log('Finished in ' + (new Date().getTime() - startedAt.getTime()) + 'ms')); //eslint-disable-line
   }
 
   b.on('error', swallowError);
@@ -50,13 +48,11 @@ function watch() {
   return rebuild();
 }
 
-gulp.task('build', function() {
-  console.log('Building ES6') // eslint-disable-line
+gulp.task('build', () => {
+  console.log('Building ES6'); // eslint-disable-line
   build();
 });
 
 gulp.task('default', ['watch']);
 
-gulp.task('watch', function() {
-  return watch();
-});
+gulp.task('watch', () => watch());
