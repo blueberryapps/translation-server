c854aaa3b192373ca1eecba6bef46f0405738c8d
Albert Němec <albert.nemec2457@gmail.com>
2017-04-10 15:38:33 +0200

    add lang_code resolution

--- app/react/browser/projects/list/components/Locale.js
+++ app/react/browser/projects/list/components/Locale.js
@@ -1,5 +1,6 @@
 import React, { PropTypes as RPT } from 'react';
 import { Flex, Box } from 'radium-flex';
+import locale from 'country-language';
 import { calculatePercents } from '../../helpers';
 
 export default class Project extends React.PureComponent {
@@ -10,9 +11,10 @@ export default class Project extends React.PureComponent {
   };
   render() {
     const { code, translationCount, translatedCount } = this.props;
+    const language = locale.getLanguage(code).name[0];
     return (
       <Flex>
-        <Box col={12}>{code}</Box>
+        <Box col={12}>{language}</Box>
         <Box col={12}>{translatedCount} / {translationCount}</Box>
         <Box col={12}>
           {calculatePercents(translatedCount, translationCount)}
