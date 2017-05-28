04224d5c5ca5fe92936d562d8e809d8194b7d50c
zepod <albert.nemec2457@gmail.com>
2017-05-28 17:50:13 +0200

    additinal review changes

--- app/react/keys/selectors.js
+++ app/react/keys/selectors.js
@@ -2,9 +2,9 @@ import { createSelector } from 'reselect';
 import { List, Map } from 'immutable';
 
 // eslint-disable-next-line no-confusing-arrow
-const getMainEntity = ({ search, page, edited }, { localeId }) => ({ keys }) => {
+const getMainEntity = ({ search = '', page, edited }, { localeId }) => ({ keys }) => {
   const lists = keys.get('lists');
-  const statePath = [localeId, edited, search || '', page];
+  const statePath = [localeId, edited, search, page];
   return !lists.isEmpty() && lists.hasIn(statePath) ?
             lists.getIn(statePath).map(id =>
               keys.getIn(['entities', 'keys', `${id}`]))
