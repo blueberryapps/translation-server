e523678ee2d0461dee2a9d343fd30ee2ce875fcd
zepod <albert.nemec2457@gmail.com>
2017-06-05 18:26:25 +0200

    create components

--- app/react/releases/transform.js
+++ app/react/releases/transform.js
@@ -1,12 +1,5 @@
 const maxLevel = 10;
-
-const findIds = (path, translations) => {
-  const key = path.join('.');
-  const values = Object.keys(translations).map(k => translations[k]);
-  return values.filter(translation => translation.key.indexOf(key) === 0).map(t => t.id);
-};
-
-const transformHierarchy = (structure: Object, translations: Object, level: number = 0, path = []): Array<Object> => {
+const transformHierarchy = (structure: Object, level: number = 0, path = []): Array<Object> => {
   if (level > maxLevel) {
     // eslint-disable-next-line no-console
     console.warn('Provided hierarchy object was to deep. Either provide different object or increase "maxLevel" variable');
@@ -15,9 +8,9 @@ const transformHierarchy = (structure: Object, translations: Object, level: numb
   return Object.keys(structure)
     .map(key => ({
       level,
-      ids: findIds([...path, key], translations),
       label: key,
-      childrenKeys: transformHierarchy(structure[key], translations, level + 1, [...path, key])
+      path: [...path, key],
+      childrenKeys: transformHierarchy(structure[key], level + 1, [...path, key])
     }));
 };
 
