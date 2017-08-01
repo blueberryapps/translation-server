274accb7535cf7516c86bc3f151b2bb062a144a2
xvie <jansuroviak@gmail.com>
2017-08-01 10:15:11 +0200

    Add return type to handle func

--- app/react/app/HeaderHome.react.js
+++ app/react/app/HeaderHome.react.js
@@ -34,7 +34,7 @@ export default class Header extends React.PureComponent {
     filterValue: ?string
   }
 
-  handleClear = () => {
+  handleClear = (): void => {
     this.props.filterProjects('');
   }
 
