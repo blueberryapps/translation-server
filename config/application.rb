223195df604d4f8aaa56e67e2d5a0dbd7526ec5c
Ceda <pleskac@gmail.com>
2019-08-20 12:05:17 +0200

    Update rails binstubs

--- config/application.rb
+++ config/application.rb
@@ -1,18 +1,22 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
+# require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
+require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
+# require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 ENVied.require(*ENV['ENVIED_GROUPS'] || Rails.groups)
 ActiveModelSerializers.config.adapter = :json
 
@@ -35,6 +39,9 @@ module TranslationServer
       end
     end
 
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -48,7 +55,5 @@ module TranslationServer
     # config.i18n.default_locale = :de
 
     config.filter_parameters += [:image]
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
--- config/puma.rb
+++ config/puma.rb
@@ -1,25 +1,34 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 1)
-min_threads = Integer(ENV['MIN_THREADS'] || 3)
-max_threads = Integer(ENV['MAX_THREADS'] || 8)
-threads min_threads, max_threads
+# Puma can serve each request in a thread from an internal thread pool.
+# The `threads` method setting takes two numbers: a minimum and maximum.
+# Any libraries that use thread pools should be configured to match
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum; this matches the default thread size of Active Record.
+#
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+threads threads_count, threads_count
 
-preload_app!
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+#
+port        ENV.fetch("PORT") { 3000 }
 
-rackup      DefaultRackup
-environment ENV['RACK_ENV'] || 'development'
+# Specifies the `environment` that Puma will run in.
+#
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-on_worker_boot do
-  ActiveRecord::Base.establish_connection
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked webserver processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-  Thread.new do
-    begin
-      ActiveRecord::Base.connection_pool.with_connection do |connection|
-        loop do
-          conn = connection.execute "NOTIFY heartbeat, 'heartbeat'"
-          sleep 2.seconds
-        end
-      end
-    ensure
-    end
-  end
-end
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+# preload_app!
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
