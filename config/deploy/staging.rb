b9781752bc253c1172661e12885fc25ac6c113f8
Ceda <pleskac@gmail.com>
2019-08-21 08:43:08 +0200

    Remove capistrano

--- config/deploy/staging.rb
+++
@@ -1,8 +0,0 @@
-set :stage,     :staging
-set :deploy_to, '/var/www/translation_server_webapps_cz'
-set :rails_env, 'staging'
-
-server 'REPLACE_ME',
-        user: 'translation_server_webapps_cz',
-        roles: %w(web app db)
-
